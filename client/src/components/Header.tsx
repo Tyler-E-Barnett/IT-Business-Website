4b161a7aa62ad8488b9c7aaf08b716d96409aac4
ReallyFunCoolDad <tylerebarnett@yahoo.com>
2025-02-01 15:22:49 -0500

    minor

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,11 +4,10 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className='flex items-end justify-start w-full h-24 p-4 bg-black'>
-      <Link to='/'>
-        <img src={logo} alt='Logo' className='h-16' />
+    <header className="flex items-end justify-start w-full h-24 p-4 bg-black">
+      <Link to="/">
+        <img src={logo} alt="Logo" className="h-16" />
       </Link>
-
       <TopNav />
     </header>
   );
