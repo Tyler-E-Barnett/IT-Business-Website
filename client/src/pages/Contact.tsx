6bc201c3d928c08bc0b12cbdaf9048d3fa548002
ReallyFunCoolDad <tylerebarnett@yahoo.com>
2025-02-19 14:38:54 -0500

    made contact more generic

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,28 +1,26 @@
 import contact from "../assets/contact.png";
 
 export default function Contact() {
+  const phone = import.meta.env.VITE_CONTACT_PHONE;
+  const email = import.meta.env.VITE_CONTACT_EMAIL;
+
   return (
     <div className="flex items-center justify-center h-full p-4 mt-auto text-left align-middle">
       <div className="m-auto">
         <img className="h-48 m-auto mt-4 mb-12" src={contact} alt="contact" />
-        <div className="">
-          <span className="font-semibold">Phone: </span>{" "}
-          <a className="text-blue-500" href="tel:203-824-4320">
-            (203) 824-4320
+        <div>
+          <span className="font-semibold">Phone: </span>
+          <a className="text-blue-500" href={`tel:${phone}`}>
+            {phone}
           </a>
         </div>
-        <div className="">
+        <div>
           <span className="font-semibold">Email: </span>
-          <a
-            className="text-blue-500"
-            href="mailto:barnetttechnologies@gmail.com"
-          >
-            barnetttechnologies@gmail.com
+          <a className="text-blue-500" href={`mailto:${email}`}>
+            {email}
           </a>
         </div>
       </div>
-
-      {/* <ProblemForm /> */}
     </div>
   );
 }
